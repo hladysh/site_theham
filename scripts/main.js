eb3a66c6a9082ff5123a9230e1e5cdb9d4eefd9a
Ivan <hladysh.im@gmail.com>
2016-12-02 09:02:42 +0000

    done 60%

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	// "Stripe::InvalidRequestError in Users::RegistrationsController#create"
+$(".tab_item").not(":first").hide();
+$(".tabs_conten .tab").click(function() {
+	$(".tabs_conten .tab").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_item").hide().eq($(this).index()).fadeIn();
+}).eq(0).addClass("active");
+
+
+
+$(".tab_items").not(":first").hide();
+$(".tabs_services_wrap .tab_services").click(function() {
+	$(".tabs_services_wrap .tab_services").removeClass("active").eq($(this).index()).addClass("active");
+	$(".tab_items").hide().eq($(this).index()).fadeIn();
+}).eq(0).addClass("active");
+
+});
